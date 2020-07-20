e14f7fad7fcee390cdc8105ea11c0479752b9a79
suguru_katsuyama <zgok105@yahoo.co.jp>
2020-07-20 19:11:52 +0900

    vue-router

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#index'
+  get '/about',   to: 'homes#index'
+  get '/contact', to: 'homes#index'
 
   namespace :api, format: 'json' do
     resources :tasks, only: [:index, :create, :update]
